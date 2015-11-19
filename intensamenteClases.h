f8a685c091671c45e83cc92620ff22ea1ed9358e
SantiagoGCandia <santi596@hotmail.com>
2015-11-18 22:16:57 -0300

    Almost done? xP
    -> Temas "Vivir" y "Descansar" terminados
    -> Falta agregar archivo de Tests

--- intensamenteClases.h
+++ intensamenteClases.h
@@ -13,15 +13,61 @@ typedef struct unRecuerdo
 	int * esCentral;
 } RECUERDO;
 
+
+// <--Cambiado unPrimerRecuerdo por PensamientosDelDia -->
+
 typedef struct unaNena 
 {
 	float nivelDeFelicidad; 
 	char * emocionDominante;
-	t_list * unPrimerRecuerdo;
+	t_list * pensamientosDelDia;
 	t_list * pensamientosCentrales;
 	t_list * procesos;
 	t_list * memoriaLargoPlazo;
 } NENA;
+
+
+
+// PROTOTYPES PRINCIPALES //
+
+void vivi_un_evento (NENA * , RECUERDO * );
+void asenta_un_recuerdo (NENA * , RECUERDO * );
+t_list * recuerdos_recientes_deldia(NENA * );
+t_list * pensamientos_dificles_deexplicar(NENA * );
+bool niega_un_recuerdo(NENA * , RECUERDO * );
+void dormir(NENA * );
+
+
+// PROTOYPES SECUNDARIOS //
+
+void convertir_recuerdo_en_pensamiento_central (NENA * , RECUERDO * );
+void disminuir_felicidad_en(NENA * , float );
+float calcular_porcentaje(float , float );
+void asentar_recuerdo_alegre(NENA * , RECUERDO * );
+void asentar_recuerdo_triste(NENA * , RECUERDO * );
+void asignarLista(NENA * );
+NENA * nena_create(char * , float * );
+RECUERDO * recuerdo_create(char * , char * );
+bool ordenar_recuerdos_porfecha_ascendente(void * , void * );
+bool ordenar_recuerdos_porfecha_descendente(void * , void * );
+bool recuerdo_dificil_deexplicar(void * );
+bool estado_deanimo_niega_unrecuerdo(char * , RECUERDO * );
+void aplicar_proceso (NENA * , void * funcion (NENA *));
+void aplicar_procesos(t_list * lista, NENA * );
+void asentar_recuerdos(NENA * );
+bool texto_contiene(void * , char * );
+void asentar_sicumple(NENA * , char * );
+void enviar_recuerdo_largo_plazo(NENA * , RECUERDO * );
+void profundizar_recuerdo(NENA * , RECUERDO * );
+void profundizar_recuerdos(NENA * );
+void perder_pensamientos_centrales_antiguos(NENA * , int );
+bool esta_en_largoplazo(t_list * , t_list * );
+bool recuerdo_repetido(NENA * );
+bool todos_con_la_misma_emocion(t_list * );
+bool recuerdos_son_emocionalmente_iguales(NENA * );
+void control_hormonal(NENA * );
+void restauracion_cognitiva(NENA * );
+void liberar_recuerdos_deldia(NENA * );
 
 
 
--- mainIntensamente.c
+++ mainIntensamente.c
@@ -39,12 +39,13 @@ void asignarLista(NENA * unaNena)
 {	t_list * recuerdos= list_create();
 	t_list * pensamientosCentrales= list_create();
 	unaNena->pensamientosCentrales= pensamientosCentrales;
-	unaNena->unPrimerRecuerdo= recuerdos;
+	unaNena->pensamientosDelDia= recuerdos;
 
 }
 
 //CREAR NENA//
-NENA * nena_create(char * emocionDominante, float * nivelDeFelicidad){
+NENA * nena_create(char * emocionDominante, float * nivelDeFelicidad)
+{
 	NENA * unaNena= malloc(sizeof(NENA));
 	asignarLista(unaNena);
 	unaNena->emocionDominante= emocionDominante;
@@ -67,7 +68,7 @@ RECUERDO * recuerdo_create(char * descripcion, char * emocion)
 	
 //*METODO VIVIUNEVENTO*//
 void vivi_un_evento (NENA * const unaNena, RECUERDO * unRecuerdo)
-{	list_add((unaNena->unPrimerRecuerdo), unRecuerdo);
+{	list_add((unaNena->pensamientosDelDia), unRecuerdo);
 }
 
 
@@ -99,8 +100,8 @@ bool ordenar_recuerdos_porfecha_descendente(void * dato1, void * dato2)
 
 
 t_list * recuerdos_recientes_deldia(NENA * const unaNena)
-{	ordenar_lista_ascendente(unaNena->unPrimerRecuerdo);
-	return (list_take(unaNena->unPrimerRecuerdo, 5));
+{	ordenar_lista_ascendente(unaNena->pensamientosDelDia);
+	return (list_take(unaNena->pensamientosDelDia, 5));
 }
 	
 //Conocer los pensamientos centrales que sean difíciles de explicar. Un recuerdo es difícil de explicar cuando su descripción tiene más de 10 palabras//
@@ -133,81 +134,180 @@ bool niega_un_recuerdo(NENA * const unaNena, RECUERDO * unRecuerdo)
 
 //Enviar a Riley a dormir, desencadenando todos sus procesos mentales//
 
-/*
-void aplicar(t_list * lista, NENA * const unaNena)
+
+
+void aplicar_proceso (NENA * const unaNena, void * funcion (NENA *))
+{
+
+funcion(unaNena);
+
+};
+
+
+
+void aplicar_procesos(t_list * lista, NENA * const unaNena)
 {
 	t_link_element *element = lista->head;
 	while (element != NULL) {
 		
-		void * funcion=(element->data);
-		funcion(unaNena);
+		aplicar_proceso(unaNena,element->data);
 		element = element->next;
-};
+	};
 }
 
-void aplicar_proceso(NENA * const unaNena)
+
+// DORMIR //
+
+
+void dormir(NENA * const unaNena)
 {
-	aplicar(unaNena->procesos, unaNena);
+	aplicar_procesos(unaNena->procesos, unaNena);
 }
 
-*/
+
+
+// ASENTAR RECUERDO //
 
 
 void asentar_recuerdos( NENA * const unaNena)
 { 
+
+	t_list * recuerdos = unaNena->pensamientosDelDia;
+	t_link_element * element = recuerdos->head;
+	while (element != NULL) {
+		
+		asenta_un_recuerdo(unaNena,element->data);
+		element = element->next;
+	};
+
 }
 	
 //ASENTAMIENTO SELECTIVO//
+
 bool texto_contiene(void * descripcion, char * palabra)
 {	char * point;
 	point= strstr(descripcion, palabra);
 	return(point != NULL);
 }
 
-void asentar_sicumple(NENA * const unaNena, RECUERDO * unRecuerdo, char * palabraClave)
-{	if(texto_contiene((unRecuerdo->descripcion), palabraClave))
-		{	asenta_un_recuerdo(unaNena, unRecuerdo);}
+void asentar_sicumple(NENA * const unaNena, char * palabraClave)
+{
+	
+	t_list * recuerdos = unaNena->pensamientosDelDia;
+	t_link_element * element = recuerdos->head;
+	
+	while (element != NULL) {
+		RECUERDO * recuerdo = element->data ;
+		if(texto_contiene((recuerdo->descripcion), palabraClave))
+		{
+			asenta_un_recuerdo(unaNena, recuerdo);
+		};
+		element = element->next;
+	};
+	
 }
 	
+	
+	
+	
 //PROFUNDIZAR RECUERDOS//	
-void profundizar_recuerdos(NENA * const unaNena)
-{
-}
+
 
 void enviar_recuerdo_largo_plazo(NENA *const unaNena, RECUERDO * unRecuerdo)
 { list_add(unaNena->memoriaLargoPlazo, unRecuerdo);}
 
 void profundizar_recuerdo(NENA * const unaNena, RECUERDO * unRecuerdo)
 {	int * x;
-	*x=1;
+	* x = 1;
 	if((unRecuerdo->esCentral != x) && (!niega_un_recuerdo(unaNena, unRecuerdo)))
 		{ enviar_recuerdo_largo_plazo(unaNena, unRecuerdo);}
 }
 
 
-//CONTROL HORMONAL//
+void profundizar_recuerdos(NENA * const unaNena)
+{
+	t_list * recuerdos = unaNena->pensamientosCentrales;
+	t_link_element * element = recuerdos->head;
+	
+	while (element != NULL) {
+		profundizar_recuerdo(unaNena, element->data);
+		element = element->next;
+	};
+	
+	
+}
+
+//CONTROL HORMONAL//  
+//								 <-- Modificado unaNena->unPrimerRecuerdo por unaNena->pensamientosCentrales -->
 
 void perder_pensamientos_centrales_antiguos(NENA * const unaNena, int valor)
 	{	int * posicion, contador=1;
-		*posicion=list_size(unaNena->unPrimerRecuerdo);
+		*posicion=list_size(unaNena->pensamientosCentrales);
 		for(contador; contador <=3; contador++)
-		{	list_remove((unaNena->unPrimerRecuerdo), *posicion);
+		{	list_remove((unaNena->pensamientosCentrales), *posicion);
 			*posicion--;	
 		}
 	}
 
-/* FALTA COMPLETAR
-bool esta_en_largoplazo()
-bool recuerdo_repetido(NENA * const unaNena)
-{	return (list_any_satisfy(unaNena->pensamientosCentrales, esta_en_largoplazo));
+//  <-- Chequear si esta bien (hasta linea 241) -->
+bool esta_en_largoplazo(t_list * pensamientosCentrales, t_list * memoriaLargoPlazo)
+{
+ t_link_element * element = pensamientosCentrales->head;
+	while (element != NULL) {
+		t_link_element * element2 = memoriaLargoPlazo->head;
+		while (element2 != NULL) {
+			if ((element->data) == (element2->data)){
+			return (1);
+			};
+		element2 = element2->next;
+		};
+	element = element->next;	
+	}
+		
 }
-*/
 
-/* FALTA TERMINAR TIENEN_LA_MISMA_EMOCION
-bool recuerdos_son_emocionalmente_iguales(NENA * const unaNena)
-{ return(list_all_satisfy((recuerdos_recientes_deldia(unaNena)), tienen_la_misma_emocion));
+
+bool recuerdo_repetido(NENA * const unaNena)
+{	
+t_list * pensamientos = unaNena->pensamientosCentrales;
+t_list * memoria = unaNena->memoriaLargoPlazo;
+
+	t_link_element * element = pensamientos->head;
+	while (element != NULL) {
+		
+		if(esta_en_largoplazo(element->data, memoria))
+		{ return (1);
+		};
+		element = element->next;
+	};
 }
-*/
+
+
+//  CONTROL HORMONAL //
+
+bool todos_con_la_misma_emocion(t_list * pensamientosDelDia)
+{
+	t_link_element * element = pensamientosDelDia->head;
+	RECUERDO * emocionDelDia = element->data;
+	while (element != NULL)
+	{
+		RECUERDO * emocionDelRecuerdo = element->data;
+		if( (emocionDelDia->emocionDominanteInstante) != (emocionDelRecuerdo->emocionDominanteInstante)  )
+		{ return (0);
+		};
+		element = element->next;
+	}
+	return (1);
+}
+
+
+bool recuerdos_son_emocionalmente_iguales(NENA * const unaNena)
+{ return(todos_con_la_misma_emocion(unaNena->pensamientosDelDia));
+}
+
+
+
+
 
 void control_hormonal(NENA * const unaNena)
 {	if((recuerdo_repetido(unaNena)) || recuerdos_son_emocionalmente_iguales(unaNena))
@@ -221,6 +321,11 @@ void restauracion_cognitiva(NENA * const unaNena)
 {	unaNena->nivelDeFelicidad= (unaNena->nivelDeFelicidad) + 100;
 }
 
-//LIBERACION DE RECUERDOS DEL DIA ** HAY QUE COMPARAR LAS FECHAS ACTUALES//
+//LIBERACION DE RECUERDOS DEL DIA 	** HAY QUE COMPARAR LAS FECHAS ACTUALES 
+// 									<-- Como que te vas a dormir todos los dias no? -->
 
+void liberar_recuerdos_deldia(NENA * const unaNena)
+{	
+	list_clean(unaNena->pensamientosDelDia);
+}
 
